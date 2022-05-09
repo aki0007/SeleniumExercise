74b0c5a888c9986de68c43a2642ef630eddb5bbc
Jaksa <jmilanovic@truveris.com>
2022-05-09 14:49:08 +0200

    Refactor POM locators

--- base/pom/navigation.py
+++ base/pom/navigation.py
@@ -3,18 +3,18 @@ import allure
 from base.webdriver import WebDriver
 
 
-class NavigationPage(object):
+class NavigationPage:
     def __init__(self, driver) -> None:
         # It is necessary to initialise driver as page class member to implement Webdriver
         self.driver: WebDriver = driver
 
-    NAVIGATION_TAB: str = "//li[descendant::a[contains(text(), '{tab}')]]"
-    SUB_NAVIGATION_TAB: str = (
+    __NAVIGATION_TAB: str = "//li[descendant::a[contains(text(), '{tab}')]]"
+    __SUB_NAVIGATION_TAB: str = (
         "//ul[@class='dropdown-menu']//li[descendant::a[contains(text(), '{tab}')]]"
     )
 
     @allure.step
     def navigation_tab_step(self, tab: str, sub_tab: str) -> None:
         # Get random user
-        self.driver.get_element(self.NAVIGATION_TAB.format(tab=tab)).click()
-        self.driver.get_element(self.SUB_NAVIGATION_TAB.format(tab=sub_tab)).click()
+        self.driver.get_element(self.__NAVIGATION_TAB.format(tab=tab)).click()
+        self.driver.get_element(self.__SUB_NAVIGATION_TAB.format(tab=sub_tab)).click()
--- base/pom/register.py
+++ base/pom/register.py
@@ -9,30 +9,30 @@ from base.data import REGISTER_DATA
 from base.helpers.utils import calculate_number_of_clicks
 
 
-class RegisterPage(object):
+class RegisterPage:
+    __CALENDAR_LEFT_ARROW: str = "//a[@data-handler='prev']"
+    __CALENDAR_SELECT_DATE: str = "//td[@data-handler='selectDay']//a[text()='{date}']"
+    __REGISTRATION_USERNAME: str = "//input[@placeholder='myusername']"
+    __REGISTRATION_EMAIL: str = "//input[@name='fld_email']"
+    __REGISTRATION_PASSWORD: str = "//input[@placeholder='Password']"
+    __REGISTRATION_CONFIRM_PASSWORD: str = "//input[@placeholder='Confirm password']"
+    __REGISTRATION_DOB: str = "//input[@placeholder='Date of birth']"
+    __REGISTRATION_PHONE: str = "//input[@placeholder='Phone']"
+    __REGISTRATION_ADDRESS: str = "//input[@placeholder='Address']"
+    __REGISTRATION_ADDRESS_TYPE: str = "//input[@name='add_type' and @value='{address_type}']"
+    __REGISTRATION_GENDER: str = "//select[@name='sex']"
+    __REGISTRATION_COUNTRY: str = "//select[@name='country']"
+    __REGISTRATION_STATE: str = "//select[@name='state']"
+    __REGISTRATION_CITY: str = "//select[@name='city']"
+    __REGISTRATION_ZIP_CODE: str = "//input[@name='zip']"
+    __REGISTRATION_TERMS_AND_CONDITIONS: str = (
+        "//em[text()='I agree with terms and conditions ']/preceding-sibling::input"
+    )
+    __SIGN_UP_BUTTON: str = "//input[@value='Sign up']"
+
     def __init__(self, driver) -> None:
         # It is necessary to initialise driver as page class member to implement Webdriver
         self.driver: WebDriver = driver
-
-    CALENDAR_LEFT_ARROW: str = "//a[@data-handler='prev']"
-    CALENDAR_SELECT_DATE: str = "//td[@data-handler='selectDay']//a[text()='{date}']"
-    REGISTRATION_USERNAME: str = "//input[@placeholder='myusername']"
-    REGISTRATION_EMAIL: str = "//input[@name='fld_email']"
-    REGISTRATION_PASSWORD: str = "//input[@placeholder='Password']"
-    REGISTRATION_CONFIRM_PASSWORD: str = "//input[@placeholder='Confirm password']"
-    REGISTRATION_DOB: str = "//input[@placeholder='Date of birth']"
-    REGISTRATION_PHONE: str = "//input[@placeholder='Phone']"
-    REGISTRATION_ADDRESS: str = "//input[@placeholder='Address']"
-    REGISTRATION_ADDRESS_TYPE: str = "//input[@name='add_type' and @value='{address_type}']"
-    REGISTRATION_GENDER: str = "//select[@name='sex']"
-    REGISTRATION_COUNTRY: str = "//select[@name='country']"
-    REGISTRATION_STATE: str = "//select[@name='state']"
-    REGISTRATION_CITY: str = "//select[@name='city']"
-    REGISTRATION_ZIP_CODE: str = "//input[@name='zip']"
-    REGISTRATION_TERMS_AND_CONDITIONS: str = (
-        "//em[text()='I agree with terms and conditions ']/preceding-sibling::input"
-    )
-    SIGN_UP_BUTTON: str = "//input[@value='Sign up']"
 
     @allure.step
     def set_up_window(self) -> None:
@@ -41,57 +41,57 @@ class RegisterPage(object):
     @allure.step
     def set_registration_personal_details(self) -> None:
         # Set username, email and password
-        self.driver.safe_send_keys(self.REGISTRATION_USERNAME, REGISTER_DATA["username"])
-        self.driver.safe_send_keys(self.REGISTRATION_EMAIL, REGISTER_DATA["email"])
-        self.driver.safe_send_keys(self.REGISTRATION_PASSWORD, REGISTER_DATA["password"])
+        self.driver.safe_send_keys(self.__REGISTRATION_USERNAME, REGISTER_DATA["username"])
+        self.driver.safe_send_keys(self.__REGISTRATION_EMAIL, REGISTER_DATA["email"])
+        self.driver.safe_send_keys(self.__REGISTRATION_PASSWORD, REGISTER_DATA["password"])
         self.driver.safe_send_keys(
-            self.REGISTRATION_CONFIRM_PASSWORD, REGISTER_DATA["password"]
+            self.__REGISTRATION_CONFIRM_PASSWORD, REGISTER_DATA["password"]
         )
         # Set date
         self.select_date(REGISTER_DATA["dob"])
         # Set phone, address, address type
-        self.driver.safe_send_keys(self.REGISTRATION_PHONE, REGISTER_DATA["phone"])
-        self.driver.safe_send_keys(self.REGISTRATION_ADDRESS, REGISTER_DATA["address"])
+        self.driver.safe_send_keys(self.__REGISTRATION_PHONE, REGISTER_DATA["phone"])
+        self.driver.safe_send_keys(self.__REGISTRATION_ADDRESS, REGISTER_DATA["address"])
 
     @allure.step
     def select_date(self, date: Sequence[str]) -> None:
         # Open calendar
-        self.driver.get_element(self.REGISTRATION_DOB).click()
+        self.driver.get_element(self.__REGISTRATION_DOB).click()
         date_obj: datetime = datetime.strptime(str(date), "%d %B %Y")
 
         # Fetch how many times left calendar arrow needs to be clicked
         number_of_clicks: int = calculate_number_of_clicks(date_obj)
         # Select correct month and year
         self.select_year_and_month_on_calendar(number_of_clicks)
-        self.driver.get_element(self.CALENDAR_SELECT_DATE.format(date=date_obj.day)).click()
+        self.driver.get_element(self.__CALENDAR_SELECT_DATE.format(date=date_obj.day)).click()
 
     def select_year_and_month_on_calendar(self, number_of_clicks: int) -> None:
         for _ in range(number_of_clicks):
-            self.driver.get_element(self.CALENDAR_LEFT_ARROW).click()
+            self.driver.get_element(self.__CALENDAR_LEFT_ARROW).click()
 
     @allure.step
     def select_register_dropdown_options(self) -> None:
         self.driver.get_element(
-            self.REGISTRATION_ADDRESS_TYPE.format(
+            self.__REGISTRATION_ADDRESS_TYPE.format(
                 address_type=REGISTER_DATA["address_type"][0]
             ),
             highlight=False,
         ).click()
         # Set gender, country, state and city
-        self.driver.select_from_dropdown_by_value(self.REGISTRATION_GENDER, REGISTER_DATA["gender"])
-        self.driver.select_from_dropdown_by_value(self.REGISTRATION_COUNTRY, REGISTER_DATA["country"])
+        self.driver.select_from_dropdown_by_value(self.__REGISTRATION_GENDER, REGISTER_DATA["gender"])
+        self.driver.select_from_dropdown_by_value(self.__REGISTRATION_COUNTRY, REGISTER_DATA["country"])
         # Page requires 3 seconds sleep
         time.sleep(3.5)
-        self.driver.select_from_dropdown_by_value(self.REGISTRATION_STATE, REGISTER_DATA["state"])
+        self.driver.select_from_dropdown_by_value(self.__REGISTRATION_STATE, REGISTER_DATA["state"])
         time.sleep(3.5)
-        self.driver.select_from_dropdown_by_value(self.REGISTRATION_CITY, REGISTER_DATA["city"])
-        self.driver.safe_send_keys(self.REGISTRATION_ZIP_CODE, REGISTER_DATA["address"])
+        self.driver.select_from_dropdown_by_value(self.__REGISTRATION_CITY, REGISTER_DATA["city"])
+        self.driver.safe_send_keys(self.__REGISTRATION_ZIP_CODE, REGISTER_DATA["address"])
 
     @allure.step
     def accept_terms_and_conditions(self) -> None:
         # Accept terms and conditions
-        self.driver.get_element(self.REGISTRATION_TERMS_AND_CONDITIONS).click()
+        self.driver.get_element(self.__REGISTRATION_TERMS_AND_CONDITIONS).click()
 
     @allure.step
     def click_sign_up_button(self) -> None:
-        self.driver.get_element(self.SIGN_UP_BUTTON).click()
+        self.driver.get_element(self.__SIGN_UP_BUTTON).click()
